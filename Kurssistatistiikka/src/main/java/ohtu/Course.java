231f175262368acc4ee1efdbc7d75f00e9aa5c76
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-11-14 20:39:36 +0200

    Coruse

---
+++ Kurssistatistiikka/src/main/java/ohtu/Course.java
@@ -0,0 +1,56 @@
+package ohtu;
+
+public class Course {
+    
+    private String name;
+    private String term;
+    private int year;
+    private String fullName;
+    private int[] exercises;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(int[] exercises) {
+        this.exercises = exercises;
+    }
+    
+    @Override
+    public String toString() {
+        return fullName + " " + term + " " + year;
+    }
+    
+}
