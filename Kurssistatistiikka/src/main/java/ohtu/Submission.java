9e2ab90e13a97acbbb0efb9bdb208e94567c141e
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-11-14 20:22:24 +0200

    Submissions

--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,14 +1,41 @@
 package ohtu;
 
 public class Submission {
+    private String course;
     private int week;
+    private int hours;
+    private int[] exercises;
+    
+    public String getCourse() {
+        return course;
+    }
+    
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getWeek() {
+        return week;
+    }
 
     public void setWeek(int week) {
         this.week = week;
     }
 
-    public int getWeek() {
-        return week;
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(int[] exercises) {
+        this.exercises = exercises;
     }
 
     @Override
