cde5980593e13dc619632bb317fd19614d80fefe
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-11-16 15:57:39 +0200

    Implement rest of test scenarios (10 done)

--- WebLogin/src/main/java/ohtu/Tester.java
+++ WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -74,6 +75,36 @@ public class Tester {
         printViewPurpose("back to front page");
         
         element = driver.findElement(By.linkText("back to home"));
+        element.click();
+        
+        System.out.println(driver.getPageSource());
+        
+        // Create new user
+        printViewPurpose("create new user");
+        
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.name("username"));
+        Random r = new Random();
+        element.sendKeys("sofia"+r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("suklaaonhyvaa");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("suklaaonhyvaa");
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+        
+        System.out.println(driver.getPageSource());
+        
+        // Logout
+        printViewPurpose("logout");
+        
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        element = driver.findElement(By.linkText("logout"));
         element.click();
         
         System.out.println(driver.getPageSource());
