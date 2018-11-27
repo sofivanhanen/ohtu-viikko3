77fb79104f66eae068f59482cb26c46071e57e77
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-11-27 10:49:54 +0200

    renames

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -2,29 +2,29 @@ package ohtu;
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int score1 = 0;
+    private int score2 = 0;
+    private String name1;
+    private String name2;
 
-    public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame(String player1, String player2) {
+        name1 = player1;
+        name2 = player2;
     }
 
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+    public void wonPoint(String player) {
+        if (player == "player1")
+            score1 += 1;
         else
-            m_score2 += 1;
+            score2 += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (score1==score2)
         {
-            switch (m_score1)
+            switch (score1)
             {
                 case 0:
                         score = "Love-All";
@@ -44,9 +44,9 @@ public class TennisGame {
                 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (score1>=4 || score2>=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = score1-score2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -56,8 +56,8 @@ public class TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = score1;
+                else { score+="-"; tempScore = score2;}
                 switch(tempScore)
                 {
                     case 0:
