d439fbb4bc661685410a340a44435d4556adf8fc
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-12-09 12:40:00 +0200

    QueryBuilder (4 done)

--- QueryLanguage/src/main/java/statistics/Main.java
+++ QueryLanguage/src/main/java/statistics/Main.java
@@ -3,16 +3,18 @@ package statistics;
 import statistics.matcher.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
-        Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
-        );
-        
+
+        Querybuilder query = new Querybuilder();
+
+        Matcher m = query.playsIn("NYR")
+                     .hasAtLeast(10, "goals")
+                     .hasFewerThan(25, "goals").build();
+
         for (Player player : stats.matches(m)) {
-            System.out.println( player );
+            System.out.println(player);
         }
     }
 }
---
+++ QueryLanguage/src/main/java/statistics/Querybuilder.java
@@ -0,0 +1,37 @@
+package statistics;
+
+import statistics.matcher.All;
+import statistics.matcher.And;
+import statistics.matcher.HasAtLeast;
+import statistics.matcher.HasFewerThan;
+import statistics.matcher.Matcher;
+import statistics.matcher.PlaysIn;
+
+public class Querybuilder {
+
+    private Matcher matcher;
+
+    public Querybuilder() {
+        this.matcher = new All();
+    }
+
+    public Matcher build() {
+        return matcher;
+    }
+
+    public Querybuilder playsIn(String team) {
+        matcher = new And(matcher, new PlaysIn(team));
+        return this;
+    }
+    
+    public Querybuilder hasAtLeast(int value, String category) {
+        matcher = new And(matcher, new HasAtLeast(value, category));
+        return this;
+    }
+    
+    public Querybuilder hasFewerThan(int value, String category) {
+        matcher = new And(matcher, new HasFewerThan(value, category));
+        return this;
+    }
+
+}
