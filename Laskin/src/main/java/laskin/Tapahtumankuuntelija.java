a5c647a7aa814dd3cb1334da66dcd4f84a00b708
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-12-09 11:46:04 +0200

    Use command (1 done)

--- Laskin/src/main/java/laskin/Tapahtumankuuntelija.java
+++ Laskin/src/main/java/laskin/Tapahtumankuuntelija.java
@@ -1,59 +1,41 @@
 package laskin;
 
+import java.util.HashMap;
+import java.util.Map;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import laskin.komennot.Erotus;
+import laskin.komennot.Komento;
+import laskin.komennot.Nollaa;
+import laskin.komennot.Summa;
 
 public class Tapahtumankuuntelija implements EventHandler {
-    private TextField tuloskentta; 
-    private TextField syotekentta; 
-    private Button plus;
-    private Button miinus;
-    private Button nollaa;
-    private Button undo;
+    private Button peru;
     private Sovelluslogiikka sovellus;
+    
+    private Map<Button, Komento> komennot;
+    private Komento edellinen = null;
 
-    public Tapahtumankuuntelija(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa, Button undo) {
-        this.tuloskentta = tuloskentta;
-        this.syotekentta = syotekentta;
-        this.plus = plus;
-        this.miinus = miinus;
-        this.nollaa = nollaa;
-        this.undo = undo;
+    public Tapahtumankuuntelija(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa, Button peru) {
+        this.peru = peru;
         this.sovellus = new Sovelluslogiikka();
+        komennot = new HashMap<>();
+        komennot.put(plus, new Summa(tuloskentta, syotekentta,  nollaa, peru, sovellus) );
+        komennot.put(miinus, new Erotus(tuloskentta, syotekentta, nollaa, peru, sovellus) );
+        komennot.put(nollaa, new Nollaa(tuloskentta, syotekentta,  nollaa, peru, sovellus) );
     }
     
     @Override
     public void handle(Event event) {
-        int arvo = 0;
- 
-        try {
-            arvo = Integer.parseInt(syotekentta.getText());
-        } catch (Exception e) {
-        }
- 
-        if (event.getTarget() == plus) {
-            sovellus.plus(arvo);
-        } else if (event.getTarget() == miinus) {
-            sovellus.miinus(arvo);
-        } else if (event.getTarget() == nollaa) {
-            sovellus.nollaa();
+        if (event.getTarget() != peru) {
+            Komento komento = komennot.get((Button)event.getTarget());
+            komento.suorita();
+            edellinen = komento;
         } else {
-            System.out.println("undo pressed");
-        }
-        
-        int laskunTulos = sovellus.tulos();
-        
-        syotekentta.setText("");
-        tuloskentta.setText("" + laskunTulos);
-        
-        if ( laskunTulos==0) {
-            nollaa.disableProperty().set(true);
-        } else {
-            nollaa.disableProperty().set(false);
-        }
-        undo.disableProperty().set(false);
+            edellinen.peru();
+            edellinen = null;
+        }        
     }
-
 }
